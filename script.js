613546fe24df45a899ebcc115701903da11c35f2
Pavel Ten <paveltenfs@gmail.com>
2022-12-31 05:51:23 +0600

    game draft

---
+++ script.js
@@ -0,0 +1,15 @@
+for (let i = 0; i < 64; i++) {
+    for (let j = 0; j < 64; j++) {
+        div = document.createElement('div');
+        container = document.querySelector("#container");
+        container.appendChild(div);
+    }
+}
+
+divs = document.querySelectorAll('#container>div');
+
+divs.forEach(div=>{div.addEventListener('mouseover', () =>
+div.classList.add('colored'))});
+
+
+
