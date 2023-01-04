7c006ceacdbc34fb6d7ebdc05b4ba84f2c41e421
Pavel Ten <paveltenfs@gmail.com>
2023-01-04 20:54:40 +0600

    created HTML and CSS layout

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 for (let i = 0; i < 64; i++) {
     for (let j = 0; j < 64; j++) {
         div = document.createElement('div');
-        container = document.querySelector("#container");
+        container = document.querySelector('#container');
         container.appendChild(div);
     }
 }
 
 divs = document.querySelectorAll('#container>div');
 
-divs.forEach(div=>{div.addEventListener('mouseover', () =>
-div.classList.add('colored'))});
-
-
-
+divs.forEach((div) => {
+    div.addEventListener('mouseover', () => div.classList.add('colored'));
+});
